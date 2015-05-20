12431202c7de73dba10e2bd2aa843c9a8620c416
Roman Savin <savin9201@gmail.com>
2015-05-20 15:21:33 +0300

    Remove unused code.
    Remove OPFPush iOS and WP8 implementations, because it will not be
    supported in the first version of plugin.
    Remove Options and Init code, because initialization is performed in
    OPFPushApplication using opfpush_config.json.

--- Unity/Assets/Plugins/OPFPush/IOPFPush.cs
+++ Unity/Assets/Plugins/OPFPush/IOPFPush.cs
@@ -2,6 +2,6 @@
 {
     public interface IOPFPush
     {
-        void Init(Options options);
+        
     }
 }
--- Unity/Assets/Plugins/OPFPush/OPFPush.cs
+++ Unity/Assets/Plugins/OPFPush/OPFPush.cs
@@ -16,14 +16,6 @@ namespace OnePF.OPFPush
         {
 #if UNITY_ANDROID
             _push = new OPFPush_Android();
-#elif UNITY_IOS
-            var push = new OPFPush_iOS();
-            push.InitFinished += delegate(bool success, string message) { if (InitFinished != null) InitFinished(success, message); };
-            _push = push;
-#elif UNITY_WP8
-            var push = new OPFPush_WP8();
-            push.InitFinished += delegate(bool success, string message) { if (InitFinished != null) InitFinished(success, message); };
-            _push = push;            
 #else
 			Debug.LogError("OPFPush is currently not supported on this platform. Sorry.");
             return;
